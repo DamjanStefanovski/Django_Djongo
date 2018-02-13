9466ab3524fc80c3d498aa3850b615e8b675c193
Damjan <damjan.stefanovski@gmail.com>
2018-02-13 10:38:28 -0500

    This is the simple project first git push

---
+++ Django_Level_Three/basicforms/basicapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.core import validators
+# Very Basic Example of a Django Form
+
+def check_for_z(value):
+    if value[0].lower() !='z':
+        raise forms.ValidationError("Name Needs to start with Z")
+
+class FormName(forms.Form):
+    name = forms.CharField(validators=[check_for_z])
+    email = forms.EmailField()
+    text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,
+                                 widget=forms.HiddenInput,
+                                 validators=[validators.MaxLengthValidator(0)])
